7e3f1d6e018e20fa30583303a9475e2729ce1879
Ravi3599 <ravikantdukre2018@gmail.com>
2022-02-23 16:56:16 +0530

    UPDATED: demostrated various http methods through rest controller

--- src/main/java/com/bridgelabz/addressbook/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbook/controller/AddressBookController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.addressbook.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,4 +19,16 @@ public class AddressBookController {
 	public String getMessage(@RequestParam String name) {
 		return "Hello "+name;
 	}
+	@PostMapping("/getMessage")
+	public String getMessage(@RequestBody AddressBook addressBook) {
+		return "Hello "+addressBook.getFirstName()+" "+addressBook.getLastName();
+	}
+	@GetMapping("/getHi/{fName}")
+	public String getHelloMessage(@PathVariable String fName) {
+		return "Hi "+fName;
+	}
+	@GetMapping("/getWelcome/{fName}")
+	public String getHelloMessage(@PathVariable String fName,@RequestParam String lName) {
+		return "Welcome "+fName+" "+lName;
+	}	
 }
